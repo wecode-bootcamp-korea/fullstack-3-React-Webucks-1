daaa93f3dda5ff1f968afaeda0a324e1d4056a1b
ebchoi <eunbichoi1@gmail.com>
2021-12-04 15:39:12 +0900

    Add: Mission 0A - 로그인 리액트로 옮기기
    - 바닐라 자바스크립트로 진행하던 위벅스 새로운 리액트 만들어서 옮기기
    - 로그인 페이지 옮기고 페이지에 레이아웃 + 스타일링 반영 확인

--- src/pages/eunbiChoi/Login/Login.js
+++ src/pages/eunbiChoi/Login/Login.js
@@ -1,6 +1,48 @@
-import React from 'react';
-function Login(){
+import { Link } from "react-router-dom";
+import "./Login.css";
 
-}
+const Login = () => {
+  return (
+    <div className="login-wrapper">
+      <h1>BeerBucks</h1>
+      <form className="login-form">
+        <div className="input-control">
+          <label className="visually-hidden" for="username"></label>
+          <input
+            className="form-items"
+            type="text"
+            name="username"
+            id="username"
+            placeholder="전화번호, 사용자 이름 또는 이메일"
+          />
+        </div>
+        <div className="input-control password-input-control">
+          <label className="visually-hidden" for="password"></label>
+          <input
+            className="form-items"
+            type="password"
+            name="password"
+            id="password"
+            placeholder="비밀번호"
+          />
+          <button className="password-button">show</button>
+        </div>
+        <button
+          tabindex="0"
+          formaction="list.html"
+          id="login-button"
+          type="submit"
+          disabled="true"
+        >
+          로그인
+        </button>
+      </form>
+      <Link className="lost-pw" to="/detail">
+        {" "}
+        비밀번호를 잊으셨나요?{" "}
+      </Link>
+    </div>
+  );
+};
 
 export default Login;
