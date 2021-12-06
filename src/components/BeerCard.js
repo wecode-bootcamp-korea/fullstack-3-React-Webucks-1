874c8cc8d6c0b5f16a7d8a40efa325f7ae745f4e
ebchoi <eunbichoi1@gmail.com>
2021-12-07 03:41:44 +0900

    Modify: change tags for beer component
    - change from description list tags to ul > li tags
    - modified scss according to html change

--- src/components/BeerCard.js
+++ src/components/BeerCard.js
@@ -1,23 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const BeerCard = ({beer}) => {
+const BeerCard = ({beer, key, alt, src}) => {
   return (
-    <div className="beer-list-item" key={beer.id}>
-      <dt>
+    <li className="beer-list-item" key={key}>
+      <div className="beer-image">
         <Link to="/detail-eunbi">
           <img
             className="beer-img"
-            src={beer.url}
-            alt={beer.name} loading="lazy"
+            src={src}
+            alt={alt} loading="lazy"
           />
         </Link>
-      </dt>
-      <dd className="beer-name">
+      </div>
+      <figcaption className="beer-name">
         {beer.name}
         <i className="fas is-favorite fa-heart fa-lg"></i>
-      </dd>
-    </div>
+      </figcaption>
+    </li>
   )
 }
 
