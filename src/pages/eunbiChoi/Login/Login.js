9a9b428fc6ce9308e1af1e1238b54746c11640c2
ebchoi <eunbichoi1@gmail.com>
2021-12-06 14:46:44 +0900

    Add: Mission 1 - 사용자 입력 데이터 저장 기능 구현
    - useState를 통해 사용자 입력 데이터 저장

--- src/pages/eunbiChoi/Login/Login.js
+++ src/pages/eunbiChoi/Login/Login.js
@@ -1,8 +1,21 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "../../../styles/reset.scss"
 import "./Login.scss";
 
+
 const Login = () => {
+  const [userid, setId] = useState();
+  const [userpw, setPw] = useState();
+  
+  function handleIdInput(e){
+    setId(e.target.value)
+  }
+  
+ function handlePwInput(e){
+   setPw(e.target.value)
+ }
+
   return (
     <div className="login-wrapper">
       <h1>BeerBucks</h1>
@@ -15,6 +28,7 @@ const Login = () => {
             name="username"
             id="username"
             placeholder="전화번호, 사용자 이름 또는 이메일"
+            onChange={handleIdInput}
           />
         </div>
         <div className="input-control password-input-control">
@@ -25,6 +39,7 @@ const Login = () => {
             name="password"
             id="password"
             placeholder="비밀번호"
+            onChange={handlePwInput}
           />
           <button className="password-button">show</button>
         </div>
@@ -39,8 +54,7 @@ const Login = () => {
         </button>
       </form>
       <Link className="lost-pw" to="/detail">
-        {" "}
-        비밀번호를 잊으셨나요?{" "}
+        비밀번호를 잊으셨나요?
       </Link>
     </div>
   );
