0c7ccea1d478e71e1197f00de39533420c214a8d
ebchoi <eunbichoi1@gmail.com>
2021-12-07 03:32:54 +0900

    Add: Footer Component
    - create Footer.js and Footer.scss

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,91 @@
+import React from 'react'
+import './Footer.scss'
+
+const Footer = () => {
+  return (
+    <footer>
+    <nav>
+      <ul className="footer-nav-list">
+        <li className="footer-nav-heading">
+          <a href="{#}">COMPANY</a>
+          <ul>
+            <li>
+              <a href="{#}">한눈에 보기</a>
+            </li>
+            <li>
+              <a href="{#}">스타벅스 사명</a>
+            </li>
+            <li>
+              <a href="{#}">스타벅스 소개</a>
+            </li>
+            <li>
+              <a href="{#}">국내 뉴스룸</a>
+            </li>
+            <li>
+              <a href="{#}">세계의 스타벅스</a>
+            </li>
+            <li>
+              <a href="{#}">글로벌 뉴스룸</a>
+            </li>
+          </ul>
+        </li>
+        <li className="footer-nav-heading">
+          <a href="{#}">CORPORATE SALES</a>
+          <ul>
+            <li>
+              <a href="{#}">단체 및 기업 구매 안내</a>
+            </li>
+          </ul>
+        </li>
+        <li className="footer-nav-heading">
+          <a href="{#}">PARTNERSHIP</a>
+          <ul>
+            <li>
+              <a href="{#}">신규 입점 제의</a>
+            </li>
+            <li>
+              <a href="{#}">협력 고객사 등록 신청</a>
+            </li>
+          </ul>
+        </li>
+        <li className="footer-nav-heading">
+          <a href="{#}">ONLINE COMMUNITY</a>
+          <ul>
+            <li>
+              <a href="{#}">페이스북</a>
+            </li>
+            <li>
+              <a href="{#}">트위터</a>
+            </li>
+            <li>
+              <a href="{#}">유튜브</a>
+            </li>
+            <li>
+              <a href="{#}">블로그</a>
+            </li>
+            <li>
+              <a href="{#}">인스타그램</a>
+            </li>
+          </ul>
+        </li>
+        <li className="footer-nav-heading">
+          <a href="{#}">RECRUIT</a>
+          <ul>
+            <li>
+              <a href="{#}">채용 소개</a>
+            </li>
+            <li>
+              <a href="{#}">채용 지원하기</a>
+            </li>
+          </ul>
+        </li>
+        <li className="footer-nav-heading">
+          <a href="{#}">WEBUCKS</a>
+        </li>
+      </ul>
+    </nav>
+    </footer>
+  )
+}
+
+export default Footer
