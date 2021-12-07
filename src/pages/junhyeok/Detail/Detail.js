756c47cf5f00dc9b02748266a1e1b121bbd446d8
LJHyeokk <clzlsqkq56@naver.com>
2021-12-07 16:07:58 +0900

    수정 - 리팩토링(props, 컴포넌트 이름으로 div바꿈)

--- src/pages/junhyeok/Detail/Detail.js
+++ src/pages/junhyeok/Detail/Detail.js
@@ -18,65 +18,65 @@ function Detail() {
   };
 
   return (
-    <div className="container">
+    <div className="detail">
       <Nav />
-      <h2>콜드 브루</h2>
-      {/* 이미지 */}
-      <div className="detail-img">
-        <img src="./images/junhyeok/coffee11.png" alt="화이트 초콜릿 모카" />
-      </div>
-      {/* 상세 데이터 */}
-      <div className="detail-info">
-        <h3 className="title">화이트 초콜릿 모카</h3>
-        <span
-          className="like"
-          onClick={() => {
-            like === '🤍' ? setlike('❤️') : setlike('🤍');
-          }}
-        >
-          {like}
-        </span>
-        <p className="smallP">White chocolate Mocha</p>
-        <p className="info">
-          달콤하고 부드러운 화이트 초콜릿 시럽과 에스프레소를 스팀 밀크와 섞어
-          휘핑크림으로 마무리한 음료로 달콤함과 강렬한 에스프레소가 부드럽게
-          어우러진 커피
-        </p>
-        <div className="item">
-          <p className="item-info">제품 영양 정보</p>
-          <span className="item-span">Tall(톨) / 355ml (12 fl oz)</span>
+      <div className="container">
+        <h2>콜드 브루</h2>
+        {/* 이미지 */}
+        <div className="detail-img">
+          <img src="./images/junhyeok/coffee11.png" alt="화이트 초콜릿 모카" />
         </div>
-        <div className="detail-kcal">
-          <Division />
-          <Division />
-        </div>
-        <div className="알러지">알레르기 유발 요인 : 우유</div>
-        <div className="review">
-          <h2>리뷰</h2>
-          {/* 댓글배열 map돌리기 */}
-          <div className="comment">
-            {inputText.map((e, i) => {
-              return (
-                <ReviewComment
-                  key={i}
-                  text={e}
-                  index={i}
-                  inputText={inputText}
-                  setinputText={setinputText}
-                />
-              );
-            })}
-          </div>
-          {/* input에 이벤트 */}
-          <input
-            maxLength="30"
-            placeholder="리뷰를 입력해주세요."
-            id="commentInput"
-            type="text"
-            onKeyDown={(e) => {
-              pressEnterEvent(e);
+        {/* 상세 데이터 */}
+        <div className="detail-info">
+          <h3 className="title">화이트 초콜릿 모카</h3>
+          <span
+            className="like"
+            onClick={() => {
+              like === '🤍' ? setlike('❤️') : setlike('🤍');
             }}
-          />
+          >
+            {like}
+          </span>
+          <p className="smallP">White chocolate Mocha</p>
+          <p className="info">
+            달콤하고 부드러운 화이트 초콜릿 시럽과 에스프레소를 스팀 밀크와 섞어
+            휘핑크림으로 마무리한 음료로 달콤함과 강렬한 에스프레소가 부드럽게
+            어우러진 커피
+          </p>
+          <div className="item">
+            <p className="item-info">제품 영양 정보</p>
+            <span className="item-span">Tall(톨) / 355ml (12 fl oz)</span>
+          </div>
+          <div className="detail-kcal">
+            <Division />
+            <Division />
+          </div>
+          <div className="알러지">알레르기 유발 요인 : 우유</div>
+          <div className="review">
+            <h2>리뷰</h2>
+            {/* 댓글배열 map돌리기 */}
+            <div className="comment">
+              {inputText.map((e, i) => {
+                return (
+                  <ReviewComment
+                    key={i}
+                    text={e}
+                    index={i}
+                    inputText={inputText}
+                    setinputText={setinputText}
+                  />
+                );
+              })}
+            </div>
+            {/* input에 이벤트 */}
+            <input
+              maxLength="30"
+              placeholder="리뷰를 입력해주세요."
+              id="commentInput"
+              type="text"
+              onKeyDown={pressEnterEvent}
+            />
+          </div>
         </div>
       </div>
       <BottomBar />
